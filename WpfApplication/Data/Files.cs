0c5ab12c8900a657b1ba08c0ae3d506b804fbd02
aenen <yaroslav.golyuk@yandex.ua>
2017-05-23 20:17:12 +0300

    First working version

---
+++ WpfApplication/Data/Files.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication.Data
+{
+    public class Files
+    {
+        public Files(string path)
+        {
+            FilePath = path;
+            Extension = Path.GetExtension(path);
+            DirectoryPath = Path.GetDirectoryName(path);
+            CurrentName = Path.GetFileNameWithoutExtension(path);
+        }
+        public string FilePath { get; set; }
+        public string Extension { get; set; }
+        public string DirectoryPath { get; set; }
+        public string CurrentName { get; set; }
+        public string NewName { get; set; }
+    }
+}
--- WpfApplication/MainWindow.xaml.cs
+++ WpfApplication/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApplication.Data;
 
 namespace WpfApplication
 {
@@ -19,10 +23,70 @@ namespace WpfApplication
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    {        
+        public ObservableCollection<Files> FileCollection { get; set; }
+        int defaultCounter = 1;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = this;
+            FileCollection = new ObservableCollection<Files>();
+        }
+
+        private void ListView_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                FileCollection.Clear();
+
+                IEnumerable<string> files = (IEnumerable<string>)e.Data.GetData(DataFormats.FileDrop);
+                foreach (var item in files)
+                {
+                    FileCollection.Add(new Files(item));
+                }
+
+                var col = (listView.View as GridView).Columns[0];
+                if (double.IsNaN(col.Width))
+                    col.Width = col.ActualWidth;
+                col.Width = double.NaN;
+                updateNewName();
+            }
+        }
+
+        private void tb_newName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            updateNewName();
+        }
+
+        void updateNewName()
+        {
+            int counter = defaultCounter;
+            for (int i = 0; i < FileCollection.Count; i++)
+            {
+                string newName = tb_newName.Text;
+                newName = Regex.Replace(newName, "[*]", counter.ToString().PadLeft(FileCollection.Count.ToString().Length, '0'));
+                FileCollection[i].NewName = newName;
+
+                counter++;
+            }
+            listView.Items.Refresh();
+
+            var col = (listView.View as GridView).Columns[1];
+            if (double.IsNaN(col.Width))
+                col.Width = col.ActualWidth;
+            col.Width = double.NaN;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in FileCollection)
+            {
+                string newPath = System.IO.Path.Combine(item.DirectoryPath, item.NewName + item.Extension);
+                File.Move(item.FilePath, newPath);
+            }
+            FileCollection.Clear();
         }
     }
 }
